e176f76b7264a4ba6368877c6bf998ba0136e99c
yazhou <fuyazhou80@hotmail.com>
2020-11-22 13:01:47 +0800

    test ssh

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
+# coding: latin-1
 
 
-str=["2015年11月5日上午，被告人胡某在平湖市乍浦镇的嘉兴市多凌金牛制衣有"]
+s=["????"]
 
-text = [s[:5] + s[-5:] for s in str]
-
-while True:
-    print(  1 )
-
-print(str[:2])
-print(str[-2:])
-
-print(text)
+print(s)
